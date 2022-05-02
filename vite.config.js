33280b203b2d74e17dee6ca2a5ca5719180e3a78
che.lin <aa10327tw@gmail.com>
2022-05-03 00:44:58 +0800

    fix: 部屬的環境變數問題

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/metawall/' : '/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, '.', 'src')
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [vue()],
+    base: mode === 'production' ? '/metawall/' : '/',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, '.', 'src')
+      }
     }
-  }
-})
+  })
+}
