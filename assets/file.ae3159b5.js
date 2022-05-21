fa46c4afe89ca038bfe9bf46a4a9366929f244e9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-05-21 12:38:44 +0000

    Deploy linche0859/metawall to github.com/linche0859/metawall.git:gh-pages

---
+++ assets/file.ae3159b5.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.79882fa7.js";const o={"Content-Type":"multipart/form-data"},n=async({params:t,payload:a})=>e.post("/file/image",a,{params:t,headers:o});export{n as p};
--- assets/track.7de0ddbc.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index.a4d8b40b.js";const c=()=>s.get("/tracks"),e=t=>s.post(`/track/${t}`),r=t=>s.delete(`/track/${t}`);export{r as d,c as g,e as p};
