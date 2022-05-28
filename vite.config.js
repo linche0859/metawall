ce4b173bf075bfb842141f26ca36b565ba29d8fb
che.lin <aa10327tw@gmail.com>
2022-05-28 21:34:41 +0800

    feat: 佈署的 public path

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default ({ mode }) => {
-  const envConfig = loadEnv(mode, './')
-  return defineConfig({
-    plugins: [vue()],
-    server: {
-      port: 3001
-    },
-    base: envConfig.VITE_PUBLISH_PATH,
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src')
-      }
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
     }
-  })
-}
+  }
+})
